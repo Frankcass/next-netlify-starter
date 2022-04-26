242c7b1d63c732ac154cd924c3838b78d736336b
Frankcass <94424913+Frankcass@users.noreply.github.com>
2022-04-25 21:30:48 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 import dynamic from "next/dynamic";
 import Link from 'next/link'
 import {useRouter} from 'next/router';
@@ -114,25 +111,6 @@ function Header({Title}){
         </nav>
     </>
   )
+}
 
 export default Header
-
-export default function Home() 
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
